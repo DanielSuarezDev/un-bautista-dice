1dffdd0689fdd8683c6fb4b6cd1ab8c81e8271d9
Daniel Suarez <danielsuarez3114@gmail.com>
2023-02-13 10:03:13 -0500

    publish blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,11 +18,11 @@ const __dirname = dirname(__filename)
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  outDir: './dist',       // When running `astro build`, path to final static output
-  publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  // root: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
+  // outDir: './dist',       // When running `astro build`, path to final static output
+  // publicDir: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
 
-  site: 'https://unbautistadice.info', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
+  site: 'https://astro-ink.vercel.app', // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   server: {
     // port: 3000,         // The port to run the dev server on.
   },
