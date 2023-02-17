a44deed545648aff8f2a7fc53f6a6f2b7ac5c4d1
Daniel Suarez <danielsuarez3114@gmail.com>
2023-02-17 10:59:13 -0500

    change branding

--- src/config.ts
+++ src/config.ts
@@ -3,31 +3,31 @@ import type{ NavItems } from './types'
 export const NAV_ITEMS: NavItems = {
     home: {
         path: '/',
-        title: 'home'
+        title: 'inicio'
     },
     blog: {
         path: '/blog',
-        title: 'blog'
+        title: 'blog & Recursos'
     },
     tags: {
         path: '/tags',
         title: 'tags'
     },
-    // media: {
-    //     path: '/media',
-    //     title: 'media'
+    // resource: {
+    //     path: '/resource',
+    //     title: 'resource'
     // },
     about: {
         path: '/about',
-        title: 'about'
+        title: 'nosotros'
     }
 }
 
 export const SITE = {
     // Your site's detail?
-    name: 'Un Baustista Dice',
-    title: 'Un Bautista Dice',
-    description: 'Compartiendo conocimiento por pasión a Cristo',
+    name: 'Impulso Cristiano',
+    title: 'Impulso Cristiano',
+    description: 'Impulsando tu fe',
     url: 'https://unbautistadice.info/',
     githubUrl: 'https://github.com/one-aalam/astro-ink',
     listDrafts: true
